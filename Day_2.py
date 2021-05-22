b781ce198e3d36415bc4700c4d505f18309e6d05
Sourav-28 <84637672+Sourav-28@users.noreply.github.com>
2021-05-22 23:29:48 +0530

    Add files via upload

---
+++ Day_2.py
@@ -0,0 +1,16 @@
+car="Fortuner"
+company="Toyota"
+cost=20.2
+year=2019
+
+print(f"In year {year} {company} is one of the top company in the market!",end=" ")
+print("It's best selling car is {} and it costs around {} lakhs.".format(car,cost))
+print()
+print(car,type(car))
+print(cost,type(cost))
+print(year,type(year))
+print()
+c1=int (cost)
+c2=float (year)
+print(c1,type(c1))
+print(c2,type(c2))
