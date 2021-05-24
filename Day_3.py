ce9ae32585b4351114b47799e5d7030f71fe5e76
Sourav-28 <84637672+Sourav-28@users.noreply.github.com>
2021-05-25 00:21:22 +0530

    Add files via upload

---
+++ Day_3.py
@@ -0,0 +1,14 @@
+# Age calculator
+a=int(input("Enter your birth year: "))
+print("Your age is: {}".format(2021-a))
+
+# Simple calculator
+q=float(input("Enter first number: "))
+w=float(input("Enter second number: "))
+print("The value of q+w is {}".format(q+w))
+print("The value of q-w is {}".format(q-w))
+print("The value of q*w is {}".format(q*w))
+print("The value of q/w is {}".format(q/w))
+print("The value of q//w is {}".format(q//w))
+print("The value of q%w is {}".format(q%w))
+print("The value of q**w is {}".format(q**w))
