8704e0664a99b550cbf8e166da67e059f8265a97
Sourav-28 <84637672+Sourav-28@users.noreply.github.com>
2021-05-27 16:08:23 +0530

    Add files via upload

---
+++ Day_5.py
@@ -0,0 +1,71 @@
+"""
+uncomment the task to run rom any of the 5 task!
+"""
+
+#1-Task 1
+
+# n=int(input("Enter a number: "))
+# for i in range(n):
+#     if i%5==0 and i%7==0:
+#         print("Number divisible by 5 and 7 are: ",i)
+
+#-2-Task 2
+
+# t=int(input("enter: "))
+# k=0
+# for i in range(1,t+1):
+#     a='2'*i
+#     print(int (a),end=" ")
+#     k=k+int(a)
+#     if i!=t:
+#         print(' + ', end=" ")
+#     else:
+#         continue
+# print()
+# print("output= ",k)
+
+#-3-Task 3
+
+# k=0
+# while True:
+#     p=input("enter the number: ")
+#     if p=='q':
+#         print("Exit successful!")
+#         break
+#     else:
+#         k=int(p)+k
+#         print("The sum is: ",k)
+
+#-4-Task 4
+
+# a=int(input("Enter a number: "))
+# b=1
+# while True:
+#     if a==0:
+#         print(0)
+#         break
+#     elif a==1:
+#         print(1)
+#         break
+#     else:
+#         for i in range(1,a+1):
+#             b=b*i
+#         print(b)
+#         break
+
+#-5-Task 5
+
+# a="python language is best programming language"
+# for i in range(len(a)):
+#     end="-"
+#     if a[i]==" ":
+#         end=" "
+#     elif i==len(a)-1:
+#         end=" "
+#     elif a[i+1]==" ":
+#         end=" "
+#     if i%2==0:
+#         print(a[i].upper(),end=end)
+#     else:
+#         print(a[i].lower(),end=end)
+
